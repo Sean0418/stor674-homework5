524df2091cdca677e0075874d733706b4a39cfca
Sean0418 <sean20030418@gmail.com>
2025-11-13 22:22:50 -0500

    Updated sbatch file

--- run_compute_bench.sh
+++ run_compute_bench.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#SBATCH -p gpu
-#SBATCH --job-name=hw5-bench
+#SBATCH -N 1
+#SBATCH --mem=5g
+#SBATCH -n 1
+#SBATCH -t 01-00:00:00
+#SBATCH -p a100-gpu
+#SBATCH --qos=gpu_access
 #SBATCH --gres=gpu:1
-#SBATCH --constraint=t4         
-#SBATCH -t 00:10:00
-#SBATCH --mem=8G
-#SBATCH --output=slurm-%j.out
+#SBATCH --output=out.%j
+#SBATCH --error=err.%j
 
 module load apptainer
 apptainer run --nv stor674-hw5_v1.sif
